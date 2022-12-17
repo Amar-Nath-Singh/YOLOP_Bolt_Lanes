e2709602c4043cad1a74dc6fd1a33215aabbbf55
Amar-Nath-Singh <nab21b005@smail.iitm.ac.in>
2022-12-17 17:41:20 +0530

    binary_stuff

--- modelrun.py
+++ modelrun.py
@@ -32,24 +32,31 @@ from lib.utils.utils import create_logger, select_device, time_synchronized
 from lib.models import get_net
 from lib.dataset import LoadImages, LoadStreams
 from lib.core.general import non_max_suppression, scale_coords
-from lib.utils import plot_one_box,show_seg_result
+from lib.utils import plot_one_box, show_seg_result
 from lib.core.function import AverageMeter
 from lib.core.postprocess import morphological_process, connect_lane
 from tqdm import tqdm
 
 from lib.utils import letterbox_for_img
 
-normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-    )
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-transform=transforms.Compose([
-            transforms.ToTensor(),
-            normalize,
-        ])
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        normalize,
+    ]
+)
+
+
+def binary_img_to_gray(img):
+    color_area = np.zeros((img.shape[0], img.shape[1], 1), dtype=np.uint8)
+    color_area[img == 1] = [255]
+    return color_area
+
 
 def get_lanes(img_det):
-    bs = 1 
+    bs = 1
     img_size = opt.img_size
     h0, w0 = img_det.shape[:2]
     img, ratio, pad = letterbox_for_img(img_det, new_shape=img_size, auto=True)
@@ -59,13 +66,13 @@ def get_lanes(img_det):
     img = np.ascontiguousarray(img)
 
     img = transform(img).to(device)
-    
+
     img = img.half() if half else img.float()  # uint8 to fp16/32
     if img.ndimension() == 3:
         img = img.unsqueeze(0)
     # Inference
     # t1 = time_synchronized()
-    det_out, da_seg_out,ll_seg_out= model(img)
+    det_out, da_seg_out, ll_seg_out = model(img)
     # t2 = time_synchronized()
     # if i == 0:
     #     print(det_out)
@@ -74,30 +81,39 @@ def get_lanes(img_det):
 
     # Apply NMS
     # t3 = time_synchronized()
-    det_pred = non_max_suppression(inf_out, conf_thres=opt.conf_thres, iou_thres=opt.iou_thres, classes=None, agnostic=False)
+    det_pred = non_max_suppression(
+        inf_out,
+        conf_thres=opt.conf_thres,
+        iou_thres=opt.iou_thres,
+        classes=None,
+        agnostic=False,
+    )
     # t4 = time_synchronized()
 
-        # nms_time.update(t4-t3,img.size(0))
-    det=det_pred[0]
+    # nms_time.update(t4-t3,img.size(0))
+    det = det_pred[0]
 
-        # save_path = str(opt.save_dir +'/'+ Path(path).name) if dataset.mode != 'stream' else str(opt.save_dir + '/' + "web.mp4")
+    # save_path = str(opt.save_dir +'/'+ Path(path).name) if dataset.mode != 'stream' else str(opt.save_dir + '/' + "web.mp4")
 
     _, _, height, width = img.shape
-    h,w,_=img_det.shape
+    h, w, _ = img_det.shape
     pad_w, pad_h = shapes[1][1]
     pad_w = int(pad_w)
     pad_h = int(pad_h)
     ratio = shapes[1][0][1]
 
-    da_predict = da_seg_out[:, :, pad_h:(height-pad_h),pad_w:(width-pad_w)]
-    da_seg_mask = torch.nn.functional.interpolate(da_predict, scale_factor=int(1/ratio), mode='bilinear')
+    da_predict = da_seg_out[:, :, pad_h : (height - pad_h), pad_w : (width - pad_w)]
+    da_seg_mask = torch.nn.functional.interpolate(
+        da_predict, scale_factor=int(1 / ratio), mode="bilinear"
+    )
     _, da_seg_mask = torch.max(da_seg_mask, 1)
     da_seg_mask = da_seg_mask.int().squeeze().cpu().numpy()
     # da_seg_mask = morphological_process(da_seg_mask, kernel_size=7)
 
-        
-    ll_predict = ll_seg_out[:, :,pad_h:(height-pad_h),pad_w:(width-pad_w)]
-    ll_seg_mask = torch.nn.functional.interpolate(ll_predict, scale_factor=int(1/ratio), mode='bilinear')
+    ll_predict = ll_seg_out[:, :, pad_h : (height - pad_h), pad_w : (width - pad_w)]
+    ll_seg_mask = torch.nn.functional.interpolate(
+        ll_predict, scale_factor=int(1 / ratio), mode="bilinear"
+    )
     _, ll_seg_mask = torch.max(ll_seg_mask, 1)
     ll_seg_mask = ll_seg_mask.int().squeeze().cpu().numpy()
     # Lane line post-processing
@@ -106,16 +122,18 @@ def get_lanes(img_det):
 
     # img_det = show_seg_result(img_det, (da_seg_mask, ll_seg_mask), _, _, is_demo=False)
 
-    img_det = show_seg_result(img_det, (da_seg_mask, ll_seg_mask), _, _, is_demo=True)
+    # img_det = show_seg_result(img_det, (da_seg_mask, ll_seg_mask), _, _, is_demo=True)
+
+    # if len(det):
+    #     det[:,:4] = scale_coords(img.shape[2:],det[:,:4],img_det.shape).round()
+    #     for *xyxy,conf,cls in reversed(det):
+    #         label_det_pred = f'{names[int(cls)]} {conf:.2f}'
+    #         plot_one_box(xyxy, img_det , label=label_det_pred, color=colors[int(cls)], line_thickness=2)
+    lanes = binary_img_to_gray(ll_seg_mask)
+    drivable = binary_img_to_gray(da_seg_mask)
+    return lanes, drivable
 
 
-    if len(det):
-        det[:,:4] = scale_coords(img.shape[2:],det[:,:4],img_det.shape).round()
-        for *xyxy,conf,cls in reversed(det):
-            label_det_pred = f'{names[int(cls)]} {conf:.2f}'
-            plot_one_box(xyxy, img_det , label=label_det_pred, color=colors[int(cls)], line_thickness=2)
-        
-    return img_det
 # def detect(cfg,opt):
 
 #     logger, _, _ = create_logger(
@@ -160,108 +178,134 @@ def get_lanes(img_det):
 
 #     inf_time = AverageMeter()
 #     nms_time = AverageMeter()
-    
+
 
 #     print('Results saved to %s' % Path(opt.save_dir))
 #     print('Done. (%.3fs)' % (time.time() - t0))
 #     print('inf : (%.4fs/frame)   nms : (%.4fs/frame)' % (inf_time.avg,nms_time.avg))
 
 
-
 def image_callback(data):
-        try:
-            image = bridge.imgmsg_to_cv2(data, "bgr8")
-            # image = cv2.cvtColor(image,cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
-            # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        except CvBridgeError as e:
-            print(e)
-        lane = get_lanes(image)
-        try:
-            output_pub.publish(bridge.cv2_to_imgmsg(lane, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
+    try:
+        image = bridge.imgmsg_to_cv2(data, "bgr8")
+        # image = cv2.cvtColor(image,cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except CvBridgeError as e:
+        print(e)
+    lanes, drivable = get_lanes(image)
+    try:
+        output_pub.publish(bridge.cv2_to_imgmsg(lanes, "bgr8"))
+    except CvBridgeError as e:
+        print(e)
+
 
 def check():
     img = cv2.imread("inference/images/3c0e7240-96e390d2.jpg")
-    cv2.imshow("Img",img)
+    cv2.imshow("Img", img)
     lan = get_lanes(img)
-    cv2.imshow("Lanes",lan)
-    cv2.waitKey(5000) 
+    cv2.imshow("Lanes", lan)
+    cv2.waitKey(5000)
+
+
 def load_video(video_path):
     cap = cv2.VideoCapture(video_path)
     time_array = []
     ini = time.time()
     print(ini)
-    while (cap.isOpened()):
+    while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            lanes = get_lanes(frame)
+            lanes, drivable = get_lanes(frame)
+            cv2.imshow("Frame", frame)
             cv2.imshow("Detected Lanes", lanes)
+            cv2.imshow("Drivable Area", drivable)
         else:
             break
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        if cv2.waitKey(25) & 0xFF == ord("q"):
             break
 
         cur = time.time()
         time_array.append(cur - ini)
         ini = cur
-    tpf = sum(time_array)/len(time_array)
-    fps = 1/tpf
+    tpf = sum(time_array) / len(time_array)
+    fps = 1 / tpf
     print(f"Avg TIme per Frames {tpf}")
     print(f"Avg FPS {fps}")
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/End-to-end.pth', help='model.pth path(s)')
-    parser.add_argument('--source', type=str, help='source')  # file/folder   ex:inference/images
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--save-dir', type=str, default='inference/output', help='directory to save results')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--load_video', type=bool, default=True)
+    parser.add_argument(
+        "--weights",
+        nargs="+",
+        type=str,
+        default="weights/End-to-end.pth",
+        help="model.pth path(s)",
+    )
+    parser.add_argument(
+        "--source", type=str, help="source"
+    )  # file/folder   ex:inference/images
+    parser.add_argument(
+        "--img-size", type=int, default=640, help="inference size (pixels)"
+    )
+    parser.add_argument(
+        "--conf-thres", type=float, default=0.25, help="object confidence threshold"
+    )
+    parser.add_argument(
+        "--iou-thres", type=float, default=0.45, help="IOU threshold for NMS"
+    )
+    parser.add_argument(
+        "--device", default="cpu", help="cuda device, i.e. 0 or 0,1,2,3 or cpu"
+    )
+    parser.add_argument(
+        "--save-dir",
+        type=str,
+        default="inference/output",
+        help="directory to save results",
+    )
+    parser.add_argument("--augment", action="store_true", help="augmented inference")
+    parser.add_argument("--update", action="store_true", help="update all models")
+    parser.add_argument("--load_video", type=bool, default=True)
     opt = parser.parse_args()
 
-
     if not opt.load_video:
-        rospy.init_node('lane_detection', anonymous=True)
+        rospy.init_node("lane_detection", anonymous=True)
         image_node = opt.source
         output_pub = rospy.Publisher("/lanes", Image, queue_size=20)
         image_sub = rospy.Subscriber(image_node, Image, image_callback)
     else:
-        print(f'Video source {opt.source}')
-
+        print(f"Video source {opt.source}")
 
     with torch.no_grad():
-        logger, _, _ = create_logger(
-        cfg, cfg.LOG_DIR, 'demo')
+        logger, _, _ = create_logger(cfg, cfg.LOG_DIR, "demo")
 
-        device = select_device(logger,opt.device)
+        device = select_device(logger, opt.device)
         if os.path.exists(opt.save_dir):  # output dir
             shutil.rmtree(opt.save_dir)  # delete dir
         os.makedirs(opt.save_dir)  # make new dir
-        half = device.type != 'cpu'  # half precision only supported on CUDA
+        half = device.type != "cpu"  # half precision only supported on CUDA
 
         # Load model
         model = get_net(cfg)
-        checkpoint = torch.load(opt.weights, map_location= device)
-        model.load_state_dict(checkpoint['state_dict'])
+        checkpoint = torch.load(opt.weights, map_location=device)
+        model.load_state_dict(checkpoint["state_dict"])
         model = model.to(device)
 
         if half:
             model.half()
-        names = model.module.names if hasattr(model, 'module') else model.names
+        names = model.module.names if hasattr(model, "module") else model.names
         colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
 
         img = torch.zeros((1, 3, opt.img_size, opt.img_size), device=device)  # init img
-        _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+        _ = (
+            model(img.half() if half else img) if device.type != "cpu" else None
+        )  # run once
         model.eval()
         if not opt.load_video:
 
             rospy.loginfo("Waiting for image topics...")
             rospy.spin()
-        
+
         else:
 
             load_video(video_path=opt.source)
